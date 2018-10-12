1c99f06aeed0a77a4bda0405bd79a840ad4e3aa3
chirag0081 <chirag.p.mca@gmail.com>
2018-10-13 00:05:34 +0530

    Change Source code First time

--- Angular2Demo/src/app/app.component.ts
+++ Angular2Demo/src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<h1>Hello This is {{name}}</h1>`,
 })
-export class AppComponent  { name = 'Angular 2!'; }
+export class AppComponent  { name = 'Angular 2 Demo!'; }
